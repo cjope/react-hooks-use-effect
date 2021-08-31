23aed382efd0c0a69caca120a8319bc4da77d231
Craig <craigjope@gmail.com>
2021-08-31 12:09:47 -0600

    commit tree 345fc12467e3cf9bb787693e781737ed7861f0ae

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [count, setCount]=useState(0)
+  const [text, setText]=useState("")
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
-  console.log("Component rendering");
+  console.log("Component rendering")
+  console.log({text})
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={e=>setCount((count)=> count+1)}>I've been clicked {count} times</button>
+      <input
+      type="text"
+      placeholder="type away"
+      value={text}
+      onChange={e=>setText(e.target.value)}
+      >
+      
+      </input>
+    </div>
+  )
 }
 
 export default App;
